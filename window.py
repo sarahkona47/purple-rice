4276cde54c11e026ef46700810514972cae08bd9
Eunice Lim <89880114+euniceyujean@users.noreply.github.com>
2023-09-25 15:18:41 -0500

    Update window.py
    window created

--- window.py
+++ window.py
@@ -1,25 +1,26 @@
 # ------------------ Import statements -------------------
 import tkinter as tk
-
+from functools import partial
 # --------------------------------------------------------
 class Window:
-    self.mainWin = tk.Tk()
+    def __init__(self):
+    
+        self.mainWin = tk.Tk()
 
-    self.otherWin = tk.Toplevel(self.mainWin)
-        self.otherWin.title("Chicken Gambling Simulator")
-        welcomeLabel = tk.Label(self.otherWin,
+        self.mainWin.title("Chicken Gambling Simulator")
+        welcomeLabel = tk.Label(self.mainWin,
                                 text="Welcome to our Chicken Gambling Simulator \n",
                                 font="Arial 24",
                                 bg="white",
                                 fg="black",
                                 justify=tk.CENTER)
         welcomeLabel.grid(row=0, column=0, columnspan = 11)
-        creatorsLabel = tk.Label(self.otherWin,
-                                    text="Created by: Sarah Choi, Daniel Seo, Eunice Lim \n",
-                                    font="Arial 18",
-                                    bg="white",
-                                    fg="black",
-                                    justify=tk.CENTER)
+        creatorsLabel = tk.Label(self.mainWin,
+                                text="Created by: Sarah Choi, Daniel Seo, Eunice Lim \n",
+                                font="Arial 18",
+                                bg="white",
+                                fg="black",
+                                justify=tk.CENTER)
     def run(self):
         """This function runs the program."""
         self.mainWin.mainloop()
