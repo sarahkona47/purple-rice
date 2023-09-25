3ab7a2ee13712fc8858fc71a77af3bbe9ca4beb2
danielseo890 <112660386+danielseo890@users.noreply.github.com>
2023-09-25 14:52:52 -0500

    Update RiggedNums.py
    added some functions that dont got anything in them for now

--- RiggedNums.py
+++ RiggedNums.py
@@ -1,1 +1,17 @@
 import timer;
+import tkinter as tk;
+import random;
+
+# Probability for timer to stop
+probability = [(1, 1.5, 0.05), (1.5, 2, 0.3), (2, 4, 0.1)] # add more probability later
+
+# Creates a timer
+def random_timer():
+    
+
+# This function stops the timer. 
+def random_timer_stop():
+
+
+# Starts the timer.
+def timer_start():
