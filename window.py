3e6cd4e1465e2082696309f330daf131011fdb31
Eunice Lim <89880114+euniceyujean@users.noreply.github.com>
2023-09-29 14:45:56 -0500

    Update window.py

--- window.py
+++ window.py
@@ -4,27 +4,12 @@ from functools import partial
 # --------------------------------------------------------
 class Window:
     def __init__(self):
-    
-        self.mainWin = tk.Tk()
-
-        self.mainWin.title("Chicken Gambling Simulator")
-        welcomeLabel = tk.Label(self.mainWin,
-                                text="Welcome to our Chicken Gambling Simulator \n",
-                                font="Arial 24",
-                                bg="white",
-                                fg="black",
-                                justify=tk.CENTER)
-        welcomeLabel.grid(row=0, column=0, columnspan = 11)
-        creatorsLabel = tk.Label(self.mainWin,
-                                text="Created by: Sarah Choi, Daniel Seo, Eunice Lim \n",
-                                font="Arial 18",
-                                bg="white",
-                                fg="black",
-                                justify=tk.CENTER)
-    def run(self):
-        """This function runs the program."""
-        self.mainWin.mainloop()
+        self.win = tk.Tk()
+        self.win.title("Chicken Gambling Simulator")
+        self.myCanvas = tk.Canvas(self.win, bg="purple", height=250, width=300)
+        self.myCanvas.pack()
+        self.win.mainloop()
 
 #----------------- Main Program --------------------------
-win = Window()
-win.run()
+window = Window()
+window.run()
