1a5e466dcece8c8204e1676be7ff42661508c8f9
Daniel <dseo@macalester.edu>
2023-10-09 15:20:51 -0500

    fixed logic but needs work

--- RiggedNums.py
+++ RiggedNums.py
@@ -38,12 +38,12 @@ def rigged_timer():
 if __name__ == "__main__":
     # Probabilities based on grouping of min - max times
     probability_ranges = [
-        {"min_time": 2, "max_time": 5, "total_probability": 0.02}, # 30% crash
-        {"min_time": 8, "max_time": 14, "total_probability": 0.2}, # 30% crash
-        {"min_time": 14, "max_time": 17, "total_probability": 0.4}, # 40% crash
-        {"min_time": 45, "max_time": 55, "total_probability": 0.2},
-        {"min_time": 60, "max_time": 70, "total_probability": 0.1},
-        {"min_time": 75, "max_time": 85, "total_probability": 0.05}
+        {"min_time": 2, "max_time": 5, "total_probability": 0.02}, # 30% crash  # currently only taking in first probability range
+        {"min_time": 8, "max_time": 14, "total_probability": 0.4}, # 40% crash
+        {"min_time": 14, "max_time": 17, "total_probability": 0.3}, # 30% crash
+        {"min_time": 18, "max_time": 25, "total_probability": 0.2}, # 20% crash
+        {"min_time": 30, "max_time": 70, "total_probability": 0.05}, # 5% crash
+        {"min_time": 75, "max_time": 100, "total_probability": 0.01} # 1% crash
     ]
 
     for range_info in probability_ranges:
@@ -52,9 +52,8 @@ if __name__ == "__main__":
         total_probability = range_info["total_probability"]
 
         num_intervals = max_time - min_time + 1
-        probability_per_interval = total_probability / num_intervals
 
-        for time_interval in range(min_time, max_time + 1):
-            probability[time_interval] = probability_per_interval
+    for time_interval in range(min_time, max_time + 1):
+        probability[time_interval] = total_probability / num_intervals
 
     rigged_timer()
