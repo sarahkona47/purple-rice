6e4a06e164359b644396be9335cbe787846a9332
Daniel <dseo@macalester.edu>
2023-10-13 15:08:41 -0500

    Reformat everything to a while loop that stores into a dictionary but it stores weird stuff right now

--- RiggedNums.py
+++ RiggedNums.py
@@ -3,9 +3,10 @@ import random
 
 # List to store intervals
 intervals = []
+previous_crashes = []
 
-def rigged_timer():
-    global intervals
+def simulate_crash():
+    global intervals, previous_crashes
 
     # Start crash
     start_time = time.time()
@@ -16,17 +17,22 @@ def rigged_timer():
         elapsed_time = current_time - start_time
 
         if elapsed_time >= intervals[0]:
-            intervals.pop(0)
+            crashed_interval = intervals.pop(0)
+            previous_crashes.append(elapsed_time)
 
         # Start from 1x incremental increase
         display_time = (elapsed_time * 0.1) + 1
         print(f"{display_time:.2f}x", end="\r")
+        
 
         # Make timer visualization smooth
         sleep_time = min(0.01, 0.5 - (current_time - start_time) % 0.5)
         time.sleep(sleep_time)
 
     print("\nCrash")
+    print("Previous crashes:")
+    for crash_time in previous_crashes:
+        print(f"Crashed at {crash_time:.2f}x")
 
 if __name__ == "__main__":
     # Probabilities based on grouping of min - max times
@@ -39,19 +45,21 @@ if __name__ == "__main__":
         {"min_time": 50, "max_time": 55, "total_probability": 0.1},  # 50-55: 10% crash
     ]
 
-    for range_info in probability_ranges:
-        min_time = range_info["min_time"]
-        max_time = range_info["max_time"]
-        total_probability = range_info["total_probability"]
+    while True:
+        for range_info in probability_ranges:
+            min_time = range_info["min_time"]
+            max_time = range_info["max_time"]
+            total_probability = range_info["total_probability"]
 
-        num_intervals = max_time - min_time + 1
+            num_intervals = max_time - min_time + 1
 
-        # Calculate the number of intervals based on the total probability
-        num_intervals_in_range = int(total_probability * num_intervals)
+            # Calculate the number of intervals based on the total probability
+            num_intervals_in_range = int(total_probability * num_intervals)
 
-        for _ in range(num_intervals_in_range):
-            intervals.append(random.randint(min_time, max_time))
+            for _ in range(num_intervals_in_range):
+                intervals.append(random.randint(min_time, max_time))
 
-    random.shuffle(intervals)  # Shuffle the intervals to randomize their order
+        random.shuffle(intervals)  # Shuffle the intervals to randomize their order
 
-    rigged_timer()
+        simulate_crash()
+        time.sleep(10)   # 10 second delay before crash starts again
