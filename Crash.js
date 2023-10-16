94476d5f3b71d181304f444b3eda013b5848c006
Eunice Lim <89880114+euniceyujean@users.noreply.github.com>
2023-10-16 16:08:39 -0500

    Added timer and multiplier, must add probability and stop timer after cashing out

--- Crash.js
+++ Crash.js
@@ -1,5 +1,4 @@
 // Declare Variables
-const intervals = [];
 const previous_crashes = [];
 const probability_ranges = [
     {"min_time": 2, "max_time": 5, "total_probability": 0.1},  // 2-5: 10% crash
@@ -10,44 +9,43 @@ const probability_ranges = [
     {"min_time": 50, "max_time": 55, "total_probability": 0.1},  // 50-55: 10% crash
     ];
 
+const earnElement = document.getElementById("earn");
+const numberInput = document.getElementById("number-input");
+const timerElement = document.getElementById("timer");
+const currentTimeElement = document.getElementById("current-time");
 
-//Sleep function
-function sleep(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms));
-  }
+let startTime = Date.now();
+let crashTime = Math.random() * 10 * 1000;
 
-// Timer function
-async function timer() {
-    const start_time = Date.now;
-    console.log("start");
+// Timer Update
+function updateTimer() {
+    const currentTime = Date.now();
+    const elapsedTime = (currentTime-startTime)/1000;
+    timerElement.textContent = elapsedTime.toFixed(2);
 
-    while (intervals >= 0){
-        const current_time = Date.now;
-        elapsed_time = new Date();
-        elapsed_time = current_time - start_time;
-        if(elapsed_time >= intervals[0]){
-            const crashed_interval = new Array();
-            crashed_interval = intervals.pop(0);
-        }
-        
-        displaytime = (elapsedTime * 0.1) + 1
-        console.log(displaytime.toFixed(2));
-
-        sleep_time = new Date();
-        sleep_time = min(0.01, 0.5 - (current_time - start_time) % 0.5)
-        await sleep(sleep_time);
-
-        console.log("\nCrash")
-        console.log("Previous crashes:")
-        for (crash_time in previous_crashes){
-            print("Crashed at", displaytime.toFixed(2), "x")
-        }
-        
+    if (elapsedTime >= crashTime) {
+        timerElement.textContent = "Crashed!";
+    } else {
+        requestAnimationFrame(updateTimer);
     }
-
+}
+   
+// Exponential Timer
+function exponentialTimer(){
+    startTime = Date.now();
+    crashTime = Math.random() * 10 * 1000;
+    updateTimer();
 }
 
+
 // Multiplier
+function multiplyNumber(){
+    const numberInput = document.getElementById("numberInput").value;
+    const parsedNumber = parseFloat(numberInput);
+    const currentTimestamp = parseFloat(timerElement.textContent);
+    
+    const earn = parsedNumber * currentTimestamp; // You can change the multiplier as needed
+    document.getElementById("earn").textContent = "You won $" + earn;
+    clearInterval(timerInterval); // Clear the timer interval after multiplying
 
-timer()
-
+}
